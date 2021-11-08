aa1de8b40cae005f24bc42406549abeae2ca19c0
Marx Costanzo Jr <marxcostanzo@yahoo.com>
2021-11-07 23:05:17 -0500

    Updated html, added scripting for image swap app

--- itis3135/scripts/Costanzo-Activity10.js
+++ itis3135/scripts/Costanzo-Activity10.js
@@ -1,15 +1,24 @@
-$(document).ready(function() {
-
-
-        // preload the image for each link
-
-        // set up the event handlers for each link
-
-    			// get the image URL and caption for each image and animate the caption
-
-            // cancel the default action of each link
-
-
-    // move the focus to the first link
-
+$(document).ready(function () {
+        $("#image_list a").each(function () {
+                var reference = $(this).attr("href");
+                var caption = $(this).attr("title");
+                // preload the image for each link
+                var currentImage = new Image();
+                currentImage.src = reference;
+                // set up the event handlers for each link
+                $(this).click(function (event) {
+                        // get the image URL and caption for each image and animate the caption
+                        $("caption, #image").fadeOut(3000,
+                                function () {
+                                        $("#image").attr("src", reference);
+                                        $("#caption").text(caption);
+                                        $("#caption, #image").fadeIn(3000);
+                                        $("#caption").animate({ fontSize: "2em" });
+                                });
+                        // cancel the default action of each link
+                        event.preventDefault();
+                });
+        });
+        // move the focus to the first link
+        $("li:first-child a").focus();
 }); // end ready
