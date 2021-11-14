07d833e1bdebb89994160708684e24be56d2c8f4
Marx Costanzo Jr <marxcostanzo@yahoo.com>
2021-11-13 23:30:53 -0500

    Completed Activity 12

---
+++ itis3135/scripts/Costanzo-Activity12.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#slider").bxSlider({
+        auto: true,
+        minSlides: 1,
+        maxSlides: 1,
+        slideWidth: 500,
+        slideMargin: 20,
+        randomStart: true,
+        captions: true,
+        pause: 3000
+    });
+});
