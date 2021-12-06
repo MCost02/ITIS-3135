ed4b05e8311d69cee932025796f16ea19b119bad
Marx Costanzo Jr <marxcostanzo@yahoo.com>
2021-12-05 19:22:28 -0500

    Updated page content and styling

---
+++ itis3135/project/scripts/services.js
@@ -0,0 +1,4 @@
+//<!-- jQuery call to the accordion() method. -->
+$(document).ready(function () {
+    $("#accordion").accordion({ active: false, collapsible: true, heightStyle: "content", });
+});
---
+++ itis3135/project/scripts/testimonials.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#slider").bxSlider({
+        auto: true,
+        minSlides: 1,
+        maxSlides: 1,
+        slideWidth: 1000,
+        slideMargin: 50,
+        randomStart: true,
+        pause: 5000
+    });
+});
