40c064ae0cbd345f14e95d74563e641594dc5bc6
Marx Costanzo Jr <marxcostanzo@yahoo.com>
2021-12-12 00:54:59 -0500

    Added AJAX functionality to services on index page

---
+++ itis3135/project/scripts/service-overview-ajax.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $.ajax({
+        type: "get",
+        url: "service-overview.json",
+        beforeSend: function(){
+            $("#serviceOverview").html("Loading...");
+        },
+        timeout: 10000,
+        error: function(xhr, status, error){
+            alert("Error: " + xhr.status + " - " + error);
+        },
+        dataType: "json",
+        success: function(data) {
+            $("#serviceOverview").html("");
+            $.each(data, function(){
+                $.each(this, function(key, value){
+                    $("#serviceOverview").append(
+                        "<h4>" + value.service + "</h4>" +
+                        "<p>" + value.description + "</p>"
+                    )
+                })
+            });
+        }
+    });
+});
